be7a5825c6df690705b489d9b2f17dc9d65c04df
luktvpl <92756992+luktvpl@users.noreply.github.com>
2022-08-26 11:07:21 +0200

    Update JsDrawingCircleProofOfConceptToImplement.js

--- JsDrawingCircleProofOfConceptToImplement.js
+++ JsDrawingCircleProofOfConceptToImplement.js
@@ -13,6 +13,30 @@ drawCircle(x,y,r,block){
     }
     this.setblock(ax,ay,block)
   }
+drawCircleScal(x,y,r,w,h,block){ //scaling circle
+    var ax = x;
+    var ay = y;
+    var ar = r;
+    for(var i = 0;i<ar;i++){
+      var sqrtemp = Math.round(Math.sqrt(ar*ar-i*i))
+      for(var wi = 0;wi<Math.round(w);wi++){
+        for(var hi = 0;hi<Math.round(h);hi++){
+        this.mirrorP(ax,Math.round(i*w)+wi,ay,Math.round(sqrtemp*h)+hi,block);
+        for(var iyfill = 0;iyfill<Math.round(sqrtemp*h)+hi;iyfill++){
+          this.mirrorP(ax,Math.round(i*w)+wi,ay,(Math.round(sqrtemp*h)+hi)-iyfill,block);
+        }
+        this.mirrorP(ay,Math.round(sqrtemp*w)+wi,ax,Math.round(i*h)+hi,block);
+        for(var ixfill = 0;ixfill<Math.round(sqrtemp*w)+wi;ixfill++){
+          this.mirrorP(ay,(Math.round(sqrtemp*w)+wi)-ixfill,ax,Math.round(i*h)+hi,block);
+        }
+        }
+      }
+      
+      
+    }
+    this.setblock(ax,ay,block)
+  }
+
   mirrorP(x,xm,y,ym,block){
     this.setblock(x+xm,y+ym,block)
     this.setblock(x+xm,y-ym,block)
